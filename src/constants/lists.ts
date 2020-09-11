ed1cce7b1bb8c3444f72921efc5d49ca355da765
CDDelta <ngjunyen@gmail.com>
2020-09-11 23:43:23 +1000

    fix app

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,5 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = window.location.origin + '/tokens.json'
+export const DEFAULT_TOKEN_LIST_URL = window.location.origin + window.location.pathname + 'tokens.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL
